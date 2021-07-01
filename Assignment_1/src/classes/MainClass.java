fddeb760317a6142e8f4dbfb25b000728729e18f
sheetalj2205 <sheetalj2205@gmail.com>
2021-07-01 20:02:24 +0530

    feat: counting number of occurrences of different words in multiple files. Created a separate class for counting words.

--- Assignment_1/src/classes/MainClass.java
+++ Assignment_1/src/classes/MainClass.java
@@ -1,10 +1,22 @@
 package classes;
 
+import java.util.*;
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		List<String> list = new ArrayList<>();
+		list.add("file1.txt");
+		list.add("file2.txt");
+
+		CountNumberOfOccurences count = new CountNumberOfOccurences();
+
+		for (String fileName : list) {
+			System.out.println(fileName);
+			count.countNoOfOccurences(fileName);
+
+		}
 
 	}
-
 }
